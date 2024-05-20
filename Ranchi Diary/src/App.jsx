54204e4cc6cdcfe5c4eee80e0f5ca9e3e37f967b
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-05-20 22:10:43 +0530

    Introducing React Router

--- Ranchi Diary/src/App.jsx
+++ Ranchi Diary/src/App.jsx
@@ -13,6 +13,7 @@ import SendNotification from "./components/SendNotification";
 import Doctors from "./components/Doctors";
 import FoodDeliviries from "./components/FoodDeliviries";
 import Tractor from "./components/Tractor";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -22,14 +23,37 @@ function App() {
         <div className="content">
           <Header />
           <div className="homepage">
-            {/* <Dashboard /> */}
-            <Tractor />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/doctor" element={<Doctors />} />
+                <Route path="/foodDeliviries" element={<FoodDeliviries />} />
+                <Route path="/payment" element={<Payment />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route
+                  path="/sendNotification"
+                  element={<SendNotification />}
+                />
+                <Route path="/subscription" element={<Subscription />} />
+                <Route path="/tractor" element={<Tractor />} />
+              </Routes>
+            </BrowserRouter>
           </div>
           {/* <Footer /> */}
         </div>
       </div>
     </>
   );
+  // return (
+  //   <>
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route path="/" element={<HomePage />} />
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </>
+  // );
 }
 
 export default App;
