3e118e3291cdaaea6fabcceca77f43dd386d5bc7
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-05-22 23:08:06 +0530

    Working on Page Layout

--- Ranchi Diary/src/components/Profile.jsx
+++ Ranchi Diary/src/components/Profile.jsx
@@ -1,13 +1,35 @@
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import img from "../../public/profilePic/Rahman.png";
+
 const Profile = () => {
   return (
-    <>
-      {/* On the right side i will display the profile pic */}
-      Name : Rahman
-      <br /> Address: Firdous Nagar , Manitola, Doranda Ranchi
-      <br /> Contact: 6203170200
-      <br /> Alternate Contact : 6203170200
-    </>
+    <span>
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image src="../../public/profilePic/Rahman.png" thumbnail />
+          </Col>
+        </Row>
+      </Container>
+      <span>
+        Name : Rahman
+        <br /> Address: Firdous Nagar , Manitola, Doranda Ranchi
+        <br /> Contact: 6203170200
+        <br /> Alternate Contact : 6203170200
+      </span>
+    </span>
   );
 };
 
 export default Profile;
+
+//  <>
+//       {/* On the right side i will display the profile pic */}
+// Name : Rahman
+// <br /> Address: Firdous Nagar , Manitola, Doranda Ranchi
+// <br /> Contact: 6203170200
+// <br /> Alternate Contact : 6203170200
+//     </>
