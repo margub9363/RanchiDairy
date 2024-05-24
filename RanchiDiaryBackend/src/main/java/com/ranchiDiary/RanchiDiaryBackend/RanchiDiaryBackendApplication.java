c8750f8bb8bb425a6e9ea530c46bcd0d0c3f9cbd
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-05-24 14:55:02 +0530

    Backend Basic ProjectSetup

---
+++ RanchiDiaryBackend/src/main/java/com/ranchiDiary/RanchiDiaryBackend/RanchiDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ranchiDiary.RanchiDiaryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanchiDiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanchiDiaryBackendApplication.class, args);
+	}
+
+}
---
+++ RanchiDiaryBackend/src/main/java/com/ranchiDiary/RanchiDiaryBackend/controller/Customer.java
@@ -0,0 +1,15 @@
+package com.ranchiDiary.RanchiDiaryBackend.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/abc")
+public class Customer {
+
+    @PostMapping("/test")
+    String testMethod(){
+        return "Hello World!";
+    }
+}
