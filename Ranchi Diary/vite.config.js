fe0d457dc5ebd1297e653932ba6f40bf93198850
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-06-18 10:29:54 +0530

    Set correct base path for GitHub Pages

--- Ranchi Diary/vite.config.js
+++ Ranchi Diary/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "RanchiDairy",
   plugins: [react()],
   build: {
     outDir: "../docs",
